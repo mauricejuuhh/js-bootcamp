64c3f86fd55e64a4a6894071ffc7c02ec34d6a8a
mauricejuuhh <mauricejuuh3@gmail.com>
2017-09-15 11:11:05 +0200

    Add files via upload

---
+++ opdracht 4/script.js
@@ -0,0 +1,15 @@
+var
+GPSLocation = 'Amsterdam centraal',
+CurrentWeather = 'cloudy',
+CurrentTemperature = 18,
+Wind = 10,
+PrecipitationChance = 10,
+Humidity = 70,
+Maintenance = true,
+name = {firstName:"Melissa", lastName:"Vriend"},
+Age = 20,
+DayOfBirth = 10,
+MonthOfBirth = 04,
+YearOfBirth = 2000,
+Student = false,
+StudentOV = false;
